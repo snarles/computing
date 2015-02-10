3ce2d559e6a14e94a8340cc7de7e69c7a571182e
Charles EC2 <root@ip-172-31-2-12.us-west-2.compute.internal>
2015-02-10 15:10:28 +0000

    changed restart script

--- examples/restart.sh
+++ examples/restart.sh
@@ -1,6 +1,10 @@
 bash /root/py27.sh
 pssh -h /root/spark-ec2/slaves bash py27.sh
-export IPYTHON_OPTS="notebook --pylab inline --ip=* --port=8888"
-bash /root/spark/sbin/stop-all.sh
+export IPYTHON_OPTS="notebook --pylab inline --profile=nbserver"
+bash /root/spark/sbin/stop-all.sh &
+echo "Paused (1)"
+read lala
 bash /root/spark/sbin/start-all.sh
+echo "Paused (2)"
+read lala
 bash /root/spark/bin/pyspark
